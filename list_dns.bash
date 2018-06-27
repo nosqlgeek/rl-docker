2ef32cac48b1ae4b1eb9c386f2c8f37b25e1dd56
David Maier <david@nosqlgeeks.de>
2018-06-27 17:32:38 +0200

    Update list_dns.bash
    Added a default DNS in case that we don't have a local one

--- list_dns.bash
+++ list_dns.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
-id=`docker ps -a | grep 'sameersbn/bind' | cut -f1 -d' '` 
-ip=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' $id`
-echo $ip
+# Try to find our local DNS, if it can't be found then return Google's name server
+
+id=`docker ps -a | grep 'sameersbn/bind' | cut -f1 -d' '`
+
+if [ "${id}x" == "x" ]
+then
+   echo 8.8.8.8
+else
+   ip=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' $id`
+   echo $ip
+fi
