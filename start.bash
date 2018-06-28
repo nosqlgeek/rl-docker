380289023caed10e4557b5155d14f15f119979e7
David Maier <david@nosqlgeeks.de>
2018-06-28 09:14:00 +0200

    Update start.bash

--- start.bash
+++ start.bash
@@ -2,12 +2,11 @@
 export DB_PORT=16379
 export NUM_NODES=3
 
+dns=`./list_dns.bash`
+echo dns = $dns
+
 for i in `seq 1 $NUM_NODES`
 do
-
-  dns=`./list_dns.bash`
-  echo dns = $dns
-
   echo "Starting container $i"
   docker run --dns=$dns -d --cap-add sys_resource --name rs-$i -p ${i}8443:8443 -p ${i}9443:9443 -p $((16379 + $i)):$DB_PORT redislabs/redis
 done
