6a38e8a2b4598d1db8865e6ccc83581fc70c853e
David Maier <david@nosqlgeeks.de>
2018-09-04 23:04:02 +0200

    Minor changes

--- list_dns.bash
+++ list_dns.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Try to find our local DNS, if it can't be found then return Google's name server
 
-id=`docker ps -a | grep 'sameersbn/bind' | cut -f1 -d' '`
+id=`docker ps -a | grep 'sameersbn/bind' | cut -f1 -d' '` 
 
 if [ "${id}x" == "x" ]
 then
