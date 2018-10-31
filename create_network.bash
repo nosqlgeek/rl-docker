b5036fe8101d820499f0a6a37b9765269d13ffc6
David Maier <david@Davids-MBP.fritz.box>
2018-10-31 09:53:49 +0100

    Fixed an issue with the CIDR

--- create_network.bash
+++ create_network.bash
@@ -4,6 +4,6 @@ source ./settings.bash
 echo "cidr = $NET_CIDR"
 echo "net = $NET_NAME"
 
-docker network create --driver=bridge --subnet=${CIDR} ${NET_NAME}
+docker network create --driver=bridge --subnet=${NET_CIDR} ${NET_NAME}
 docker network ls | grep ${NET_NAME}
 
