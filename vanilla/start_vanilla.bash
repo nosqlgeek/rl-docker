42eb699a999b2f790534bebe7ff02af337df0360
David Maier <david@nosqlgeeks.de>
2018-11-09 23:38:07 +0100

    Modified the settings of the vanilla ubuntu-server container

--- vanilla/start_vanilla.bash
+++ vanilla/start_vanilla.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --network rs-net --ip 172.1.0.2 --dns=172.1.0.201 -d --cap-add sys_resource --name van-1 -p 38443:8443 -p 39443:9443 -p 16679:16379 ubuntu-server
+docker run -it --network rs-net --ip 172.1.0.191 --dns=172.1.0.201 -d --cap-add sys_resource --name van-1 -p 8443:8443 -p 9443:9443 -p 16379:16379 ubuntu-server
