9d345a2dd80bc98914c03c00a17fed853b1b8408
David Maier <david@nosqlgeeks.de>
2018-11-20 10:32:02 +0100

    Using global settings for the vanialla instance, too

--- vanilla/start_vanilla.bash
+++ vanilla/start_vanilla.bash
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run -it --network rs-net --ip 172.1.0.191 --dns=172.1.0.201 -d --cap-add sys_resource --name van-1 -p 8443:8443 -p 9443:9443 -p 16379:16379 ubuntu-server
+source ../settings.bash
+
+## Calculate the IP
+net_ip_pre=`echo $NET_CIDR | cut -f 1 -d'/' | cut -f 1-3 -d'.'`
+net_ip=${net_ip_pre}.191
+echo ip = $net_ip
+
+docker run -it --network $NET_NAME --ip $net_ip --dns=172.1.0.201 -d --cap-add sys_resource --name van-1 -p 8443:8443 -p 9443:9443 -p 16379:16379 ubuntu-server
