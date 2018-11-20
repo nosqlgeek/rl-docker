54d0b3f3a0627bf6447c98a0997e874cf4024748
David Maier <david@nosqlgeeks.de>
2018-11-20 10:30:22 +0100

    Added Prometheus support

---
+++ start_prometheus.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+source ./settings.bash
+
+## Calculate the IP
+net_ip_pre=`echo $NET_CIDR | cut -f 1 -d'/' | cut -f 1-3 -d'.'`
+net_ip=${net_ip_pre}.202
+echo ip = $net_ip
+
+cp prometheus.yml /tmp/prometheus.yml
+docker run --network $NET_NAME --ip $net_ip -p 9090:9090 -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
--- training/start_prometheus.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp prometheus.yml /tmp/prometheus.yml
-docker run --network rs-net --ip 172.1.0.181 -p 9090:9090 -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
