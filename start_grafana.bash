8d456f36a1932d4d0bdda410d9f69b7580d32d7d
David Maier <david@nosqlgeeks.de>
2019-02-15 16:23:51 +0100

    Added some Grafana stuff

--- start_grafana.bash
+++ start_grafana.bash
@@ -3,8 +3,8 @@ source ./settings.bash
 
 ## Calculate the IP
 net_ip_pre=`echo $NET_CIDR | cut -f 1 -d'/' | cut -f 1-3 -d'.'`
-net_ip=${net_ip_pre}.202
+net_ip=${net_ip_pre}.211
 echo ip = $net_ip
 
 cp prometheus.yml /tmp/prometheus.yml
-docker run --network $NET_NAME --ip $net_ip -p 9090:9090 -v /tmp/prometheus.yml:/etc/prometheus/prometheus.yml prom/prometheus
+docker run --network $NET_NAME --ip $net_ip -d -p 3000:3000 grafana/grafana 
