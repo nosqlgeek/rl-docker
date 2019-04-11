7c7b1f112772eb28ff4d2166bc046f9db146c6bf
David Maier <david@nosqlgeeks.de>
2019-04-11 13:50:28 +0200

    Fixed issue

--- start_portainer.bash
+++ start_portainer.bash
@@ -6,4 +6,4 @@ net_ip_pre=`echo $NET_CIDR | cut -f 1 -d'/' | cut -f 1-3 -d'.'`
 net_ip=${net_ip_pre}.212
 echo ip = $net_ip
 
-docker run --network $NET_NAME --ip $net_ip -d -p 9000:9000 portainer/portainer
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer --network $NET_NAME --ip $net_ip -d -p 9000:9000 portainer/portainer
