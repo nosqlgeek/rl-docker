c0b335c41038f9c67ddb25e5ff6b933faf909d45
David Maier <david@nosqlgeeks.de>
2020-01-10 14:16:33 +0100

    Updated the fix_dns script to reflect that the built-in DNS does now listen by default on port 5300, but it should listen on 53

--- fix_dns.bash
+++ fix_dns.bash
@@ -7,6 +7,8 @@ echo dns = $dns
 
 ./list.bash | cut -f1 -d' ' | while read i
 do
-  docker exec $i bash -c "echo nameserver $dns > /etc/resolv.conf"
-  docker exec $i bash -c "echo search $FQN >> /etc/resolv.conf"  
+  docker exec -u 0 $i bash -c "echo nameserver $dns > /etc/resolv.conf"
+  docker exec -u 0 $i bash -c "echo search $FQN >> /etc/resolv.conf"  
+  docker exec -u 0 $i bash -c "sed -i 's/local-port=5300/local-port=53/g' /opt/redislabs/config/pdns.conf"
+  docker exec -u 0 $i bash -c "/opt/redislabs/bin/supervisorctl restart pdns_server"
 done
